e781b484d10d02a89f99d8f3f9785c745e147c7a
siddiswar <sidda005ait@gmail.com>
2026-02-22 11:17:03 +0000

    delete tem folders

--- .aws-sam/build/ComputeYesterdayDt/app.py
+++
@@ -1,9 +0,0 @@
-from datetime import datetime, timedelta
-from zoneinfo import ZoneInfo
-
-
-def lambda_handler(event, context):
-    london = ZoneInfo("Europe/London")
-    today = datetime.now(london).date()
-    dt = (today - timedelta(days=1)).isoformat()
-    return {"dt": dt}
